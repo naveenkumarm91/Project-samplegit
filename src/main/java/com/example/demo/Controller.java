b808c0640f331f2d3dba4eaa21bdbdb9da33dc04
naveenkumarm91 <naveenkumarm0891@gmail.com>
2025-08-18 22:55:20 +0800

    init commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -4,6 +4,7 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Controller {
+    @GetMapping("/")
     public ResponseEntity<?> getGreets(@RequestParam("names")String names){
         return new ResponseEntity<>("Resolver-help"+names, HttpStatus.OK);
     }
